bc083c4bb0532f99b4fd626edc376de19f5e0247
Anderson Ferreira Canel <andersonfcanel@gmail.com>
2018-11-04 21:16:35 -0200

    testando

--- src/com/suam/AFD/AFD.java
+++ src/com/suam/AFD/AFD.java
@@ -12,15 +12,11 @@ public class AFD {
 	static HashMap<Integer, String> conjuntoDeEstadosFinaisMap = new HashMap<Integer, String>();
 
 	public static void main(String[] args) {
-		String[] nulos = { "{", "}", "," };// identificando carateres de formatação do conjunto
-
 		// ENTRADA DO CONJUNTO DE SIMBOLOS (ALFABETO)
 		String alfabeto = "{a,b}";// Enrando com conjundo de caracteres(alfabeto)
-		String conjuntodeSimbolos_AlfabetoImprime = alfabeto;
+		String alfabetoImprime = alfabeto;
 
-		for (String n : nulos) {
-			alfabeto = alfabeto.replace(n, "");
-		}
+		alfabeto = removeNulos(alfabeto);
 
 		List<Character> alf = new ArrayList<>(); // ***********
 		Character[] conjuntodeSimbolos_Alfabeto = new Character[alfabeto.length()];
@@ -34,38 +30,34 @@ public class AFD {
 
 		// ENTRADA DO CONJUNTO DE ESTADOS
 		String conjuntoDeEstadosTerminaisEnaoTerminais = "{A,B,C,D,E}";
-		String conjuntoDeEstadosTerminaisEnaoTerminaisImprime = conjuntoDeEstadosTerminaisEnaoTerminais;
+		String conjuntoDeEstadosTerminaisImprime = conjuntoDeEstadosTerminaisEnaoTerminais;
 
-		// removendo carateres de formatação do conjunto
-		for (String n : nulos) {
-			conjuntoDeEstadosTerminaisEnaoTerminais = conjuntoDeEstadosTerminaisEnaoTerminais.replace(n, "");
-		}
+		conjuntoDeEstadosTerminaisEnaoTerminais = removeNulos(conjuntoDeEstadosTerminaisEnaoTerminais);
 
 		int estados = conjuntoDeEstadosTerminaisEnaoTerminais.length();
 		int[] conjuntoDeEstados = new int[estados];
 		int a = 0;
 		for (Character ch : conjuntoDeEstadosTerminaisEnaoTerminais.toCharArray()) {
 			conjuntoDeEstadosMap.put(a, ch.toString());
-
+			System.out.println("Estado: " + ch + " - Chave: " + a);
 			conjuntoDeEstados[a] = a;
 			a++;
-			System.out.println("Estado: " + ch);
+
 		}
 
 		// ENTRADA FUNCAO DE TRANSICAO
-
 		String[] funcDelta = new String[conjuntodeSimbolos_Alfabeto.length
 				* conjuntoDeEstadosTerminaisEnaoTerminais.length()];
-		funcDelta[0] = "A1,a;B2";
-		funcDelta[1] = "A1,b;C3";
-		funcDelta[2] = "B2,a;B2";
-		funcDelta[3] = "B2,b;D4";
-		funcDelta[4] = "C3,a;B2";
-		funcDelta[5] = "C3,b;C3";
-		funcDelta[6] = "D4,a;B2";
-		funcDelta[7] = "D4,b;E5";
-		funcDelta[8] = "E5,a;B2";
-		funcDelta[9] = "E5,a;C3";
+		funcDelta[0] = "A,a;B";
+		funcDelta[1] = "A,b;C";
+		funcDelta[2] = "B,a;B";
+		funcDelta[3] = "B,b;D";
+		funcDelta[4] = "C,a;B";
+		funcDelta[5] = "C,b;C";
+		funcDelta[6] = "D,a;B";
+		funcDelta[7] = "D,b;E";
+		funcDelta[8] = "E,a;B";
+		funcDelta[9] = "E,a;C";
 
 		String[] estadoPartidaS = new String[funcDelta.length];
 		String[] caracConsumidoS = new String[funcDelta.length];
@@ -74,8 +66,6 @@ public class AFD {
 		int[] estadoPartida = new int[funcDelta.length];
 		int[] estadoDestino = new int[funcDelta.length];
 		char[] le = new char[funcDelta.length];
-
-		// -->>
 
 		for (int i = 0; i < funcDelta.length; i++) {
 			if (funcDelta[i] == null) {
@@ -88,89 +78,101 @@ public class AFD {
 			estadoDestinoS[i] = p1[1];
 		}
 
-		int i = 0;
-		int regra = 0;
-
 		for (int p = 0; p < funcDelta.length; p++) {
+			String aux = estadoPartidaS[p];
 
-			String aux = estadoPartidaS[i];
-			estadoPartida[i] = Integer.parseInt(String.valueOf(aux.charAt(1)));
+			int h = 0;
+			for (Character ch : conjuntoDeEstadosTerminaisEnaoTerminais.toCharArray()) {
+				System.out.println("H: " + h + "==> p =" + p);
+				for (int j = 0; j < (conjuntodeSimbolos_Alfabeto.length
+						* conjuntoDeEstadosTerminaisEnaoTerminais.length()); j++) {
 
-			aux = estadoDestinoS[i];
-			estadoDestino[i] = Integer.parseInt(String.valueOf(aux.charAt(1)));
+					if (estadoPartidaS[j].equals(ch.toString())) {
+						System.out.println("J1: " + j);
+						estadoPartida[j] = h;
+					}
+				}
+				for (int j = 0; j < (conjuntodeSimbolos_Alfabeto.length
+						* conjuntoDeEstadosTerminaisEnaoTerminais.length()); j++) {
 
-			aux = caracConsumidoS[i];
-			le[i] = aux.charAt(0);
-			i++;
+					if (estadoDestinoS[j].equals(ch.toString())) {
+						System.out.println("J2: " + j);
+						estadoDestino[j] = h;
+					}
+				}
+				h++;
+			}
+
+			aux = caracConsumidoS[p];
+			le[p] = aux.charAt(0);
 		}
 
 		// ----APOS O TRATAMENTO----------
-		System.out.println("ESTADO PARTIDA: " + Arrays.toString(estadoPartida));
+		System.out.println("ESTADO PARTIDA:     " + Arrays.toString(estadoPartida));
 		System.out.println("CARACTER CONSUMIDO: " + Arrays.toString(le));
-		System.out.println("ESTADO DESTINO: " + Arrays.toString(estadoDestino));
+		System.out.println("ESTADO DESTINO:     " + Arrays.toString(estadoDestino));
 
 		// -------------------------------------------------
-		// ENTRA COM ESTADO INICIAL
 
+		// ENTRA COM ESTADO INICIAL
 		String estadoIni = "{A}";
 		int estadoi = 0;
-		// removendo carateres de formatação do conjunto
-		for (String n : nulos) {
-			conjuntoDeEstadosTerminaisEnaoTerminais = conjuntoDeEstadosTerminaisEnaoTerminais.replace(n, "");
-		}
+
+		estadoIni = removeNulos(estadoIni);
 
 		int u = 0;
-		for (Character ch : alfabeto.toCharArray()) {
+		for (Character ch : conjuntoDeEstadosTerminaisEnaoTerminais.toCharArray()) {
 			if (ch.toString().equals(estadoIni)) {
+				System.out.println("Estado Inicial: " + conjuntoDeEstadosMap.get(u) + " - posisao: " + u);
 				estadoi = u;
-				System.out.println("Estado Inicial: " + conjuntoDeEstadosMap.get(ch));
+				break;
 			}
 			u++;
 		}
 
 		// -------------------------------------------------
 		// ENTRA COM ESTADOS FINAIS
-		String conjuntoEstadosTerminais = "{E}";
+		String conjuntoEstadosTerminais = "{C,D,E}";
 
-		// removendo carateres de formatação do conjunto
-		for (String n : nulos) {
-			conjuntoEstadosTerminais = conjuntoEstadosTerminais.replace(n, "");
-		}
+		conjuntoEstadosTerminais = removeNulos(conjuntoEstadosTerminais);
 
 		int[] estadosf = new int[conjuntoEstadosTerminais.length()];
-		int b = 0;
+		int b = 0, y = 0;
+
 		for (Character ch : conjuntoDeEstadosTerminaisEnaoTerminais.toCharArray()) {
 			for (Character ch1 : conjuntoEstadosTerminais.toCharArray()) {
-				if (conjuntoDeEstadosMap.get(b).equals(ch1.toString())) {
-					conjuntoDeEstadosFinaisMap.put(b, ch1.toString());
-					System.out.println("Estado Final: " + ch1);
+				if (conjuntoDeEstadosMap.get(y).equals(ch1.toString())) {
+					System.out.println("Estado Final: " + ch1 + " - posicao: " + y);
+					estadosf[b] = y;
+					b++;
+					break;
 				}
 			}
-			b++;
+			y++;
 		}
 
-		int tamanhoConjuntoEstadosTerminais = b;
+		System.out.println("ESTADOS FINAIS: " + Arrays.toString(estadosf));
 
 		// -------------------------------------------------
-		imprimirAutomato(conjuntodeSimbolos_AlfabetoImprime, conjuntoDeEstadosTerminaisEnaoTerminaisImprime,
-				estadoPartida, estadoDestino, le, estadoIni, conjuntoEstadosTerminais);
+		imprimirAutomato(alfabetoImprime, conjuntoDeEstadosTerminaisImprime, estadoPartida, estadoDestino, le,
+				estadoIni, conjuntoEstadosTerminais);
 		// -------------------------------------------------
 
-		// ------------------INSERE E VERIFICA PALAVRA
+		// INSERE E VERIFICA PALAVRA
 		String palavraS;
 		boolean flagPal;
 		// PALAVRAS
 		do {
 			int teste = 0;
-			
+
 			palavraS = JOptionPane.showInputDialog(null,
 					"Entre com a palavra a ser verificada: \nPara conferir suas entradas digite anteriores digite 'I'\nPara sair digite s");
 			if (palavraS.equalsIgnoreCase("s")) {
 				break;
 			}
 			if (palavraS.equalsIgnoreCase("I")) {
-				imprimirAutomato(conjuntodeSimbolos_AlfabetoImprime, conjuntoDeEstadosTerminaisEnaoTerminaisImprime,
-						estadoPartida, estadoDestino, le, estadoIni, conjuntoEstadosTerminais);
+				imprimirAutomato(alfabetoImprime, conjuntoDeEstadosTerminaisImprime, estadoPartida, estadoDestino, le,
+						estadoIni, conjuntoEstadosTerminais);
 				palavraS = JOptionPane.showInputDialog(null,
 						"Entre com a palavra a ser verificada: \nPara conferir suas entradas digite anteriores digite 'I'\nPara sair digite s");
 				if (palavraS.equalsIgnoreCase("s")) {
@@ -180,11 +182,8 @@ public class AFD {
 
 			// -----VALIDAÃ‡ÃƒO DA PALAVRA
 			flagPal = VerificaPalavra(palavraS, conjuntodeSimbolos_Alfabeto);
-			System.out.println("PALAVRA: "+palavraS);
-			System.out.println("ARRAY DE CHAR insere: " + Arrays.toString(conjuntodeSimbolos_Alfabeto));
-			
+
 			if (!flagPal) {
-				System.out.println("INVALIDA PALAVRA");
 			} else {
 
 				char[] palavra = palavraS.toCharArray();
@@ -193,12 +192,11 @@ public class AFD {
 
 				for (int p = 0; p < palavra.length; p++) {
 					int mudou = 0;
-					int j = 0;
-					System.out.println("conjuntodeSimbolos_Alfabeto: "+conjuntodeSimbolos_Alfabeto[p]);
 					for (int k = 0; k < funcDelta.length; k++) {
-						
+						// System.out.println("ENTROU:" + (k + 1));
 						if ((palavra[p] == le[k]) && (estadoa == estadoPartida[k])) {
 							aux = estadoDestino[k];
+							System.out.println("Estado de destino ==>" + aux);
 							mudou = 1;
 							break;
 						} else {
@@ -208,12 +206,18 @@ public class AFD {
 					}
 
 					if (mudou == 1) {
+						System.out.println("Estado anterior:  " + estadoa);
 						estadoa = aux;
+						System.out.println("NOVO Estado anterior:  " + estadoa);
 					} else {
 						break;
 					}
 
-					for (int k = 0; k < tamanhoConjuntoEstadosTerminais; k++) {
+					for (int k = 0; k < conjuntoEstadosTerminais.length(); k++) {
+						System.out.println("***********************COMPARANDO");
+						System.out.println("Estado anterio: " + estadoa);
+						System.out.println("Estado final: " + estadosf[k]);
+						System.out.println("**********************************");
 						if (estadoa == estadosf[k]) {
 							teste = 1;
 						} else {
@@ -235,7 +239,6 @@ public class AFD {
 	}
 
 	// ---------------METODOS-------------------------------------
-	// ---- METODOS DE IMPRESSÃƒO DEINFORMAÃ‡OES -
 
 	// IMPRIME ENTRADAS
 	private static void imprimirAutomato(String alf, String est, int[] estadoPartida, int[] estadoDestino, char[] le,
@@ -276,7 +279,7 @@ public class AFD {
 	private static boolean VerificaPalavra(String palavra, Character[] alf) {
 		int cont = 0;
 		for (int x = 0; x < palavra.length(); x++) {
-			Character caracPalavra =  palavra.charAt(x);
+			Character caracPalavra = palavra.charAt(x);
 			for (int y = 0; y < alf.length; y++) {
 				if (caracPalavra.equals(alf[y])) {
 					cont++;
@@ -285,13 +288,22 @@ public class AFD {
 		}
 
 		if (cont == palavra.length()) {
-			// JOptionPane.showMessageDialog(null, "TODOS OS SIMBOLOS PERTENCEM
-			// AO ALFABETO!!!\n\n");
 			return true;
 		} else {
-			JOptionPane.showMessageDialog(null, "A palavra "+palavra+" contém simbolos não pertencentes ao conjunto de simbolos (alfabeto)!n",
+			JOptionPane.showMessageDialog(null,
+					"A palavra " + palavra + " contém simbolos não pertencentes ao conjunto de simbolos (alfabeto)!n",
 					"WARNING", JOptionPane.WARNING_MESSAGE);
 			return false;
 		}
+	}
+
+	// removendo carateres de formatação do conjunto
+	public static String removeNulos(String conjunto) {
+		String[] nulos = { "{", "}", "," };// identificando carateres de formatação do conjunto
+		for (String n : nulos) {
+			conjunto = conjunto.replace(n, "");
+		}
+		return conjunto;
+
 	}
 }
